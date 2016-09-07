bef1a3abddfcb3dd5e29c03317eb1f1cef48faaf
kalebo <kaleb.olson@gmail.com>
2016-09-07 17:01:57 -0600

    Added functions to persist credentials to file securely Please enter the commit message for your changes. Lines starting

---
+++ src/credential_functions.ps1
@@ -0,0 +1,9 @@
+﻿function Store-SecureCredential($Path) {
+$Credential = Get-Credential
+$Credential | Export-CliXml $path
+}
+
+function Import-SecureCredential($Path) {
+$Credential = Import-Clixml -Path $Path
+return $Credential
+}
