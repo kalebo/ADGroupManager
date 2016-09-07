1f477ab70523d4811b1e281ee5864301debc1f16
kalebo <kaleb.olson@gmail.com>
2016-09-07 17:17:18 -0600

    Updated variable names and cleaned up some formating

--- src/populate_grps.ps1
+++ src/populate_grps.ps1
@@ -1,20 +1,23 @@
 param (
     [Parameter(Mandatory=$true)][string]$WorkingDir,
-    [PSCredential]$BYUCredential = (Get-Credential),
-    [PSCredential]$OLDCredential = (Get-Credential)
+    [PSCredential]$ADCredential = (Get-Credential),
+    [PSCredential]$FileAccessCredential = (Get-Credential)
 )
 
 Import-module ActiveDirectory
 . "C:\ADGroupManager\src\list_functions.ps1"
+. "C:\ADGroupManager\src\credential_functions.ps1"
 
 $WorkingDir = Resolve-Path $WorkingDir
 
-Pull-Lists -SourceDir "\\luna\c$\Inetpub\Websites\WWW Physics Web Site\App_Data\groups" -WorkingDir $WorkingDir -Credential $OLDCredential
+Pull-Lists -SourceDir "\\luna\c$\Inetpub\Websites\WWW Physics Web Site\App_Data\groups" `
+           -WorkingDir $WorkingDir `
+           -Credential $FileAccessCredential
 
 $csv_files = Get-ChildItem $WorkingDir -Filter physics-grp-*.csv
 $csv_files | ForEach-Object {
 	Write-Host "Processing $_ ..." -ForegroundColor cyan
-	Regenerate-ADGroup -File $_ -Credential $BYUCredential
+	Regenerate-ADGroup -File $_ -Credential $ADCredential
 }
 Clean-ListDir $WorkingDir
 
