0493d5ec5a011d7b550bfae9433a40faa2352861
kalebo <kaleb.olson@gmail.com>
2016-09-21 15:59:32 -0600

    updates

--- src/populate_grps.ps1
+++ src/populate_grps.ps1
@@ -1,14 +1,33 @@
+[CmdletBinding(DefaultParameterSetName="simple")]
+
 param (
     [Parameter(Mandatory=$true)][string]$WorkingDir,
-    [PSCredential]$ADCredential = (Get-Credential),
-    [PSCredential]$FileAccessCredential = (Get-Credential)
+    $ADCredentialFile,
+    $FileAccessCredentialFile
 )
+
 
 Import-module ActiveDirectory
 . "C:\ADGroupManager\src\list_functions.ps1"
 . "C:\ADGroupManager\src\credential_functions.ps1"
 
+
 $WorkingDir = Resolve-Path $WorkingDir
+
+
+if (!$ADCredentialFile){
+    [PSCredential]$ADCredential = (Get-Credential)
+} else {
+    $ADCredential = Import-SecureCredential -Path $ADCredentialFile
+}
+
+if (!$FileAccessCredentialFile) {
+    [PSCredential]$FileAccessCredential = (Get-Credential)
+} else {
+    $FileAccessCredential = Import-SecureCredential -Path $FileAccessCredentialFile
+}
+
+
 
 Pull-Lists -SourceDir "\\luna\c$\Inetpub\Websites\WWW Physics Web Site\App_Data\groups" `
            -WorkingDir $WorkingDir `
