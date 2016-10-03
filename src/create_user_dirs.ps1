cbed3ba0f1637275d0829df617d499fb45b57d12
kalebo <kaleb.olson@gmail.com>
2016-10-03 16:41:01 -0600

    Added a script to generate user directories for a group in AD

---
+++ src/create_user_dirs.ps1
@@ -0,0 +1,24 @@
+﻿
+$AllUsersGroupName = "physics-grp-users"
+$UserDirs = "E:\Users\"
+$AllUsers = Get-ADGroupMember $AllUsersGroupName -Recursive
+
+function Setup-UserFolder($UserName, $Path) {
+
+    $UserPath = Join-Path $Path $UserName
+    if (!(Test-Path $UserPath)) {
+        New-Item -ItemType directory -Path $Path -Name $UserName
+        echo $UserPath
+
+        $Acl = Get-Acl $UserPath
+        $AccessRule  = New-Object System.Security.AccessControl.FileSystemAccessRule($UserName, "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow")
+        $Acl.SetAccessRule($AccessRule)
+  
+        Set-Acl $UserPath $Acl
+    }
+}
+
+$AllUsers | ForEach-Object {
+    Setup-UserFolder -UserName $_.SamAccountName -Path $UserDirs
+}
+
